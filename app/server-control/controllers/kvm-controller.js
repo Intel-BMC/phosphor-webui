985ec34d87dec2d387858ef7f5cbfba2ee25c650
kathy <kathryn.elainex.pine@intel.com>
2019-10-06 09:40:13 -0700

    Resize KVM for smaller display
    -KVM screen displays 100% of content width when browser is narrow or browser
    width is reduced, rather than being clipped, using CSS.
    -KVM screen reduces height based on browser size as well.
    -Added option to override this setting with toggle button.
    -Built upon Ed's patch 22478
    
    CSS-only overrides to the #noVNC_container>div>canvas were applied and
    resize the display in terms of the browser height & width.
    
    However, the changes to resize the display using CSS overrides alone do not
    allow the mouse to track properly on the actual server. Therefore, changes to
    rfb.scaleViewport & rfb.clipViewport also used (from Ed's patch 22478).
    
    Tested:
    Resized browser width and height, and KVM display resized properly. Changed
    toggle to autoscale off and on, and retested. Mouse tracked properly in all
    scenarios when verified using external KVM alongside display.
    
    Change-Id: I91c2c296cb028a516994a0d06bc265c907838a2a
    Signed-off-by: kathy <kathryn.elainex.pine@intel.com>

--- app/server-control/controllers/kvm-controller.js
+++ app/server-control/controllers/kvm-controller.js
@@ -12,9 +12,10 @@ window.angular && (function(angular) {
   'use strict';
 
   angular.module('app.serverControl').controller('kvmController', [
-    '$scope', '$location', '$log',
-    function($scope, $location, $log) {
+    '$scope', 'dataService', '$location', '$log',
+    function($scope, dataService, $location, $log) {
       var rfb;
+      $scope.autoscale = true;
 
       $scope.$on('$destroy', function() {
         if (rfb) {
@@ -34,13 +35,13 @@ window.angular && (function(angular) {
         $log.debug('RFB disconnected');
       }
 
-      var host = $location.host();
-      var port = $location.port();
+      var host = dataService.server_id;
+
       var target =
           angular.element(document.querySelector('#noVNC_container'))[0];
 
       try {
-        rfb = new RFB(target, 'wss://' + host + ':' + port + '/kvm/0', {});
+        rfb = new RFB(target, 'wss://' + host + '/kvm/0', {});
 
         rfb.addEventListener('connect', connected);
         rfb.addEventListener('disconnect', disconnected);
@@ -50,6 +51,25 @@ window.angular && (function(angular) {
             null, 'fatal', null, 'Unable to create RFB client -- ' + exc);
         return;  // don't continue trying to connect
       };
+
+      $scope.windowreload = function(autoscale) {
+        if (autoscale) {
+          var myEl =
+              angular.element(document.querySelector('#noVNC_container'));
+          myEl.addClass('enforceAdjustment');
+          // reload to force autoscale when toggled twice; TODO: could check
+          // height and only reload if window is less than height of noVNC
+          location.reload();
+        };
+      };
+      // on toggle, class "enforceAdjustment" override needed
+      // However the class must be removed on resize to keep cursor tracking
+      $scope.set_rfbScale = function(displayScale) {
+        rfb.clipViewport = displayScale;
+        rfb.scaleViewport = displayScale;
+      };
+
+      $scope.set_rfbScale($scope.autoscale);
     }
   ]);
 })(angular);
