36a26ac9191d487f6e73e0dd24fa0d1d49b863f1
kathy <kathryn.elainex.pine@intel.com>
2019-10-09 09:20:43 -0700

    Update sensors page function calls, improve sorting styling
    Fixes problem with proper loading being dependant on ambiguous and
    non-reliable time delay, which was keeping all sensors from being shown when
    loading was too slow
    - Rebuilt functions to load in order to ensure data is complete before handled
    - Removed time delays from sensors page
    - Applied a function to apply proper reading units more efficiently
    - Improved table cell formatting for sort order arrows (also applied same
    styling to event log table header)
    
    Change-Id: I658b7a176bec80e162e7fb3ad57c4e8a2857985c
    Signed-off-by: kathy <kathryn.elainex.pine@intel.com>

--- app/server-health/controllers/sensors-overview-controller.js
+++ app/server-health/controllers/sensors-overview-controller.js
@@ -11,48 +11,35 @@ import {_} from 'core-js';
 window.angular && (function(angular) {
   'use strict';
   angular.module('app.overview').controller('sensorsOverviewController', [
-    '$scope', '$q', '$window', '$filter', '$timeout', 'APIUtils',
-    'toastService', 'dataService', 'Constants',
+    '$scope', '$q', '$window', '$filter', 'APIUtils', 'toastService',
+    'dataService', 'Constants',
     function(
-        $scope, $q, $window, $filter, $timeout, APIUtils, toastService,
-        dataService, Constants) {
+        $scope, $q, $window, $filter, APIUtils, toastService, dataService,
+        Constants) {
       $scope.dataService = dataService;
       $scope.dropdown_selected = false;
 
-      $scope.loading = false;
-      $scope.componentList = [];
+      $scope.loading = true;
       $scope.sensorsInfo = {};
       $scope.fullSensorsInfo = [];
-      $scope.selectedComponent = {};
-      $scope.showThresholds = true;
+      $scope.showThresholds = false;
       $scope.customSearch = '';
       $scope.searchTerms = [];
       $scope.messages = Constants.MESSAGES.SENSOR;
-
       $scope.selectedSeverity = 'all';
       $scope.severityList = ['All', 'Critical', 'Warning', 'Ok'];
-
-      $scope.filteredVoltSensors = [];
-      $scope.filteredTempSensors = [];
-      $scope.filteredFanSensors = [];
-      $scope.filteredSevereSensors = [];
-      $scope.severeNodesCount = 0;
-      $scope.warningNodesCount = 0;
+      $scope.severes = 0;
+      $scope.warnings = 0;
       $scope.suppressAlerts = false;
-      $scope.alertloadMessage = '';
       $scope.sensorsInfo.Temperatures = [];
       $scope.sensorsInfo.Fans = [];
       $scope.sensorsInfo.Voltages = [];
-      $scope.sensorsInfo.Merged = [];
       $scope.selectedChoice = 'All';
-      $scope.resultsShown = false;
       $scope.reverseSeverity = false;
-      $scope.reverse = true;
+      $scope.reverse = false;
       $scope.keyname = 'Name';
-      $scope.myEl = angular.element(document.querySelector('#sensor-message'));
-      $scope.myEl.addClass('sensor-hide');
+
       $scope.export_name = 'sensors.json';
-      $scope.loading = false;
 
       $scope.jsonData = function(data) {
         var dt = {};
@@ -60,6 +47,127 @@ window.angular && (function(angular) {
           dt[item.original_data.key] = item.original_data.value;
         });
         return JSON.stringify(dt);
+      };
+
+      $scope.showAlert = function() {
+        var alertText = '';
+        $scope.severes =
+            $filter('filter')($scope.mergedSensors, 'critical').length;
+        $scope.warnings =
+            $filter('filter')($scope.mergedSensors, 'warning').length;
+
+        if (($scope.severes > 0 || $scope.warnings > 0) &&
+            !$scope.suppressAlerts) {
+          if ($scope.severes) {
+            alertText = $scope.severes;
+            $scope.severes > 1 ? alertText = alertText + ' sensors' :
+                                 alertText = alertText + ' sensor';
+            alertText = alertText + ' at <b>critical</b> status level.<BR>';
+          };
+          if ($scope.warnings) {
+            alertText = alertText + $scope.warnings;
+            $scope.warnings > 1 ? alertText = alertText + ' sensors' :
+                                  alertText = alertText + ' sensor';
+            alertText = alertText + ' at <b>warning</b> status level.';
+          };
+          $scope.severes ? toastService.alert(alertText) :
+                           toastService.warning(alertText);
+
+          $scope.suppressAlerts = true;
+        };
+      };
+
+      // FIRST
+      $scope.loadSensorData = function() {
+        var i = 0;
+        var chassisListTotal = 2;
+        APIUtils.getAllChassisCollection().then(
+            function(chassisList) {
+              chassisListTotal = chassisList.length;
+              angular.forEach(chassisList, function(chassis) {
+                i = i + 1;
+                var resData = getComponentSensors(chassis, chassisListTotal, i);
+                $scope.fullSensorsInfo.push(resData);
+              });
+            },
+            function(error) {
+              console.log(JSON.stringify(error));
+            });
+      };
+
+      // SECOND
+      function getComponentSensors(component, totalChassis, curChassis) {
+        var data = component;
+        data['sensors'] = {'Temperatures': [], 'Fans': [], 'Voltages': []};
+
+        APIUtils.getSensorsInfo(component.Power['@odata.id'])
+            .then(function(res) {
+              if (res.hasOwnProperty('Voltages')) {
+                data.sensors['Voltages'] = res.Voltages;
+              }
+              APIUtils.getSensorsInfo(component.Thermal['@odata.id'])
+                  .then(function(res) {
+                    if (res.hasOwnProperty('Temperatures')) {
+                      data.sensors['Temperatures'] = res.Temperatures;
+                    }
+                    if (res.hasOwnProperty('Fans')) {
+                      data.sensors['Fans'] = res.Fans;
+                    }
+                    return;
+                  })
+                  .finally(function() {
+                    if (curChassis == totalChassis) {
+                      $scope.loading = false;
+
+                      // Prepare Export file
+                      $scope.loadMergedSensors().then(function() {
+                        $scope.showAlert();
+                        $scope.mergedsensorsexport =
+                            ((JSON.stringify($scope.sensorsInfo.Voltages.concat(
+                                  $scope.sensorsInfo.Fans,
+                                  $scope.sensorsInfo.Temperatures)))
+                                 .replace('[', ''))
+                                .replace(']', '');
+                        // encode # ins export
+                        var i = 0;
+                        var strLength = $scope.mergedsensorsexport.length;
+                        for (i; i < strLength; i++) {
+                          $scope.mergedsensorsexport =
+                              $scope.mergedsensorsexport.replace('#', '%23');
+                        };
+                      });
+                    };
+                  });
+              return;
+            })
+
+        return data;
+      };
+
+      // THIRD
+      $scope.loadMergedSensors = function() {
+        var deferred = $q.defer();
+        // Flattened sensor data to display all sensors
+        // Looping through all chassis collections to flatten sensors data
+        angular.forEach($scope.fullSensorsInfo, function(record) {
+          $scope.sensorsInfo.Temperatures = [].concat(
+              $scope.sensorsInfo.Temperatures, record.sensors.Temperatures);
+          $scope.sensorsInfo.Fans =
+              [].concat($scope.sensorsInfo.Fans, record.sensors.Fans);
+          $scope.sensorsInfo.Voltages =
+              [].concat($scope.sensorsInfo.Voltages, record.sensors.Voltages);
+        });
+
+        // FOURTH: merge all sensors into one array
+        $scope.mergedSensors = $scope.sensorsInfo.Voltages.concat(
+            $scope.sensorsInfo.Fans, $scope.sensorsInfo.Temperatures);
+        deferred.resolve($scope.mergedSensors);
+        return deferred.promise;
+      };
+
+      $scope.clear = function() {
+        $scope.customSearch = '';
+        $scope.searchTerms = [];
       };
 
       $scope.doSearchOnEnter = function(event) {
@@ -76,6 +184,35 @@ window.angular && (function(angular) {
         }
       };
 
+      $scope.showReadingUnits = function(
+          reading, readingVolts, readingCelsius, readingUnits) {
+        var readings = [reading, readingVolts, readingCelsius];
+        for (var i = 0; i < readings.length; i++) {
+          if (readings[i]) {
+            return $filter('number')(readings[i], 2) +
+                $scope.getReadingUnits(
+                    readingVolts, readingCelsius, readingUnits);
+          };
+        };
+      };
+
+      $scope.getReadingUnits = function(
+          readingVolts, readingCelsius, readingUnits) {
+        var sStr = '';
+        if (readingVolts) {
+          sStr = sStr + 'V';
+        };
+
+        if (readingCelsius) {
+          sStr = sStr + '\xB0' +
+              ' C';
+        };
+        if (readingUnits) {
+          sStr = readingUnits.replace('Percent', '%');
+        }
+        return sStr;
+      };
+
       $scope.doSearchOnClick = function() {
         var search =
             $scope.customSearch.replace(/^\s+/g, '').replace(/\s+$/g, '');
@@ -90,13 +227,6 @@ window.angular && (function(angular) {
         }
       };
 
-
-      $scope.toggleSeverity = function(severity) {
-        severity = $filter('lowercase')(severity)
-        $scope.selectedSeverity = severity;
-      };
-
-
       $scope.filterBySeverity = function(sensor) {
         if ($scope.selectedSeverity == 'all') return true;
         return (
@@ -110,92 +240,12 @@ window.angular && (function(angular) {
 
       $scope.filterBySearchTerms = function(sensor) {
         if (!$scope.searchTerms.length) return true;
-      };
-
-
-      $scope.loadMergedSensors = function() {
-        $scope.loading = true;
-        // Flattened sensor data to display all sensors.
-        $scope.sensorsInfo = {'Temperatures': [], 'Fans': [], 'Voltages': []};
-        // Looping through all chassis collections to flattened sensors data
-        angular.forEach($scope.fullSensorsInfo, function(record) {
-          $scope.sensorsInfo.Temperatures = [].concat(
-              $scope.sensorsInfo.Temperatures, record.sensors.Temperatures);
-          $scope.sensorsInfo.Fans =
-              [].concat($scope.sensorsInfo.Fans, record.sensors.Fans);
-          $scope.sensorsInfo.Voltages =
-              [].concat($scope.sensorsInfo.Voltages, record.sensors.Voltages);
-        });
-
-
-
-        $scope.loading = false;
+        for (var i = 0, length = $scope.searchTerms.length; i < length; i++) {
+          var search_text = sensor.Name.toLowerCase();
+          if (search_text.indexOf($scope.searchTerms[i].toLowerCase()) == -1)
+            return false;
+        }
         return true;
-      };
-
-      $scope.toggleSeverity = function(severity) {
-        severity = $filter('lowercase')(severity);
-        $scope.selectedSeverity = severity;
-      };
-
-      // delay due to non-asynchronous call; TODO: set promise and
-      // $q.all with one finally
-      $timeout(function() {
-        $scope.showAlert();
-        $scope.myEl.removeClass('sensor-hide');
-      }, 1800);
-
-      $scope.filterBySeverity = function(sensor) {
-        if ($scope.selectedSeverity == 'all') return true;
-        return (
-            ((sensor.Status.Health == 'OK') &&
-             ($scope.selectedSeverity == 'ok')) ||
-            ((sensor.Status.Health == 'Warning') &&
-             $scope.selectedSeverity == 'warning') ||
-            ((sensor.Status.Health == 'Critical') &&
-             $scope.selectedSeverity == 'critical'));
-      };
-
-
-      $scope.$watch('toggle', function() {
-        //  $scope.toggleText = $scope.toggle ? 'Hide Thresholds' : 'Show
-        //  Thresholds';
-        $scope.toggleText = 'Show Thresholds';
-        $scope.toggle ? $scope.showThresholds = true :
-                        $scope.showThresholds = false;
-      })
-
-      $scope.showAlert = function() {
-        $scope.severeNodesCount =
-            $filter('filter')($scope.mergedSensors, 'critical').length;
-        $scope.warningNodesCount =
-            $filter('filter')($scope.mergedSensors, 'warning').length;
-
-        if (($scope.severeNodesCount > 0 || $scope.warningNodesCount > 0) &&
-            !$scope.suppressAlerts) {
-          if ($scope.severeNodesCount) {
-            $scope.alertloadMessage = $scope.severeNodesCount;
-            $scope.severeNodesCount > 1 ?
-                $scope.alertloadMessage = $scope.alertloadMessage + ' nodes' :
-                $scope.alertloadMessage = $scope.alertloadMessage + ' node';
-            $scope.alertloadMessage = $scope.alertloadMessage +
-                ' at <b>critical</b> status level.<BR>';
-          };
-          if ($scope.warningNodesCount) {
-            $scope.alertloadMessage =
-                $scope.alertloadMessage + $scope.warningNodesCount;
-            $scope.warningNodesCount > 1 ?
-                $scope.alertloadMessage = $scope.alertloadMessage + ' nodes' :
-                $scope.alertloadMessage = $scope.alertloadMessage + ' node';
-            $scope.alertloadMessage =
-                $scope.alertloadMessage + ' at <b>warning</b> status level.';
-          };
-          $scope.severeNodesCount ?
-              toastService.alert($scope.alertloadMessage) :
-              toastService.warning($scope.alertloadMessage);
-
-          $scope.suppressAlerts = true;
-        };
       };
 
       $scope.sortBy = function(keyname) {
@@ -231,119 +281,6 @@ window.angular && (function(angular) {
         } else {
           $scope.filterByComponent = val;
         }
-      };
-
-      $scope.loadMergedSensors = function() {
-        $scope.loading = true;
-        // Flattened sensor data to display all sensors.
-        $scope.sensorsInfo = {'Temperatures': [], 'Fans': [], 'Voltages': []};
-        // Looping through all chassis collections to flattened sensors data
-        angular.forEach($scope.fullSensorsInfo, function(record) {
-          $scope.sensorsInfo.Temperatures = [].concat(
-              $scope.sensorsInfo.Temperatures, record.sensors.Temperatures);
-          $scope.sensorsInfo.Fans =
-              [].concat($scope.sensorsInfo.Fans, record.sensors.Fans);
-          $scope.sensorsInfo.Voltages =
-              [].concat($scope.sensorsInfo.Voltages, record.sensors.Voltages);
-        });
-        // merge all sensors into one array
-        // delay due to non-asynchronous call; TODO: set promise and $q.all
-        // with one finally
-        $timeout(function() {
-          $scope.mergedSensors = $scope.sensorsInfo.Voltages.concat(
-              $scope.sensorsInfo.Fans, $scope.sensorsInfo.Temperatures);
-        }, 1500);
-        $scope.loading = false;
-      };
-
-      function getComponentSensors(component) {
-        var data = component;
-        data['sensors'] = {'Temperatures': [], 'Fans': [], 'Voltages': []};
-        APIUtils.getSensorsInfo(component.Thermal['@odata.id'])
-            .then(function(res) {
-              if (res.hasOwnProperty('Temperatures')) {
-                data.sensors['Temperatures'] = res.Temperatures;
-              }
-              if (res.hasOwnProperty('Fans')) {
-                data.sensors['Fans'] = res.Fans;
-              }
-              return;
-            })
-            .finally(function() {
-              $scope.loadMergedSensors();
-
-              // delay due to non-asynchronous call; TODO: set promise and
-              // $q.all with one finally
-              $timeout(function() {
-                $scope.showAlert();
-                $scope.myEl.removeClass('sensor-hide');
-              }, 1800);
-
-              $scope.loading = false;
-            });
-        APIUtils.getSensorsInfo(component.Power['@odata.id'])
-            .then(function(res) {
-              if (res.hasOwnProperty('Voltages')) {
-                data.sensors['Voltages'] = res.Voltages;
-              }
-              return;
-            })
-        return data;
-      };
-
-      $scope.$watch('toggle', function() {
-        $scope.toggleText = 'Show Thresholds';
-        $scope.toggle ? $scope.showThresholds = true :
-                        $scope.showThresholds = false;
-      });
-
-      $scope.showAlert = function() {
-        $scope.severeNodesCount =
-            $filter('filter')($scope.mergedSensors, 'critical').length;
-        $scope.warningNodesCount =
-            $filter('filter')($scope.mergedSensors, 'warning').length;
-
-        if (($scope.severeNodesCount > 0 || $scope.warningNodesCount > 0) &&
-            !$scope.suppressAlerts) {
-          if ($scope.severeNodesCount) {
-            $scope.alertloadMessage = $scope.severeNodesCount;
-            $scope.severeNodesCount > 1 ?
-                $scope.alertloadMessage = $scope.alertloadMessage + ' sensors' :
-                $scope.alertloadMessage = $scope.alertloadMessage + ' sensor';
-            $scope.alertloadMessage = $scope.alertloadMessage +
-                ' at <b>critical</b> status level.<BR>';
-          };
-          if ($scope.warningNodesCount) {
-            $scope.alertloadMessage =
-                $scope.alertloadMessage + $scope.warningNodesCount;
-            $scope.warningNodesCount > 1 ?
-                $scope.alertloadMessage = $scope.alertloadMessage + ' sensors' :
-                $scope.alertloadMessage = $scope.alertloadMessage + ' sensor';
-            $scope.alertloadMessage =
-                $scope.alertloadMessage + ' at <b>warning</b> status level.';
-          };
-          $scope.severeNodesCount ?
-              toastService.alert($scope.alertloadMessage) :
-              toastService.warning($scope.alertloadMessage);
-
-          $scope.suppressAlerts = true;
-        };
-      };
-
-
-
-      $scope.loadSensorData = function() {
-        $scope.loading = true;
-        APIUtils.getAllChassisCollection().then(
-            function(chassisList) {
-              angular.forEach(chassisList, function(chassis) {
-                var resData = getComponentSensors(chassis);
-                $scope.fullSensorsInfo.push(resData);
-              });
-            },
-            function(error) {
-              console.log(JSON.stringify(error));
-            })
       };
 
       $scope.loadSensorData();
